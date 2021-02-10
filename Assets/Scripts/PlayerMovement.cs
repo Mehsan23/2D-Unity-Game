ede4d31cdf64e75c372dacb065a992597e46bf4b
MEhsanShah <MehsanS23@users.noreply.github.com>
2021-02-11 02:04:54 +0300

    New level added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float moveSpeed = 5f;
     public bool isGrounded = false;
-
+    public Animator myAnim;
     void Start () {
-
+        myAnim = GetComponent<Animator>();
     }
 
 
@@ -16,13 +16,21 @@ public class PlayerMovement : MonoBehaviour {
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        myAnim.SetFloat("Speed", Mathf.Abs(movement.x));
     }
 
     void Jump(){
         if (Input.GetButtonDown("Jump") && isGrounded == true){
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+
         }
-
-
-    }
+        if (Input.GetButtonDown("Jump") == true){
+            myAnim.SetBool("isJump", true);
+        }
+        if (Input.GetButtonDown("Jump") == false && isGrounded == true){
+            myAnim.SetBool("isJump", false);
+        }
+         
+    }    
 }
